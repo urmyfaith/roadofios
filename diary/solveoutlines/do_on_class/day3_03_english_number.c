bbbf6ad531dc4a980634f8724c0f691ebf57bf87
urmyfaith <urmyfaith@qq.com>
2014-11-05 15:17:11 +0800

    english_number

--- diary/solveoutlines/do_on_class/day3_03_english_number.c
+++ diary/solveoutlines/do_on_class/day3_03_english_number.c
@@ -6,8 +6,12 @@ int main(void)
 	scanf("%d",&ch);
 	
 	if( (ch>64 && ch<90) ||(ch>96&&ch<123))
-		printf("YES");
+		printf("YES\n");
 	else
-		printf("NO");
+		printf("NO\n");
 	return 0;
 }
+
+#if 0
+	if(ch>='A'&& ch <='Z' || ch >='a'&& ch <='z')
+#endif
