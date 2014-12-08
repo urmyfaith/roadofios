19bef2198feaea34c464b5e106c1b7b3155a43da
urmyfaith <urmyfaith@qq.com>
2014-12-08 08:49:42 +0800

    rename UI_porject_of_ios

---
+++ UIProjects/1208_week6_day1/1208_week6_day1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  1208_week6_day1
+//
+//  Created by zx on 12/8/14.
+//  Copyright (c) 2014 zx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
