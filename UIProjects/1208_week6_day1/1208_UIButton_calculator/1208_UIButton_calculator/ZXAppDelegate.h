ce907ef170f2b3247a2c6c8afb55708d7792d4ea
urmyfaith <urmyfaith@qq.com>
2014-12-09 00:38:18 +0800

    modify

--- UIProjects/1208_week6_day1/1208_UIButton_calculator/1208_UIButton_calculator/ZXAppDelegate.h
+++ UIProjects/1208_week6_day1/1208_UIButton_calculator/1208_UIButton_calculator/ZXAppDelegate.h
@@ -1,8 +1,9 @@
+
 //
 //  ZXAppDelegate.h
 //  1208_UIButton_calculator
 //
-//  Created by zx on 12/9/14.
+//  Created by zx on 12/8/14.
 //  Copyright (c) 2014 zx. All rights reserved.
 //
 
@@ -11,5 +12,12 @@
 @interface ZXAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property (nonatomic,strong) NSMutableString *expression;
+
+@property (nonatomic,assign) float expressionValue;
+
+@property (nonatomic,strong) UILabel *showLabel;
+
 
 @end
