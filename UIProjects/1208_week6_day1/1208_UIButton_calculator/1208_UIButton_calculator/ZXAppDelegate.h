7ee7fe806ff9d4490f40c9833a9260cfeb8773a6
urmyfaith <urmyfaith@qq.com>
2014-12-09 00:36:59 +0800

    recover deleted project

---
+++ UIProjects/1208_week6_day1/1208_UIButton_calculator/1208_UIButton_calculator/ZXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZXAppDelegate.h
+//  1208_UIButton_calculator
+//
+//  Created by zx on 12/9/14.
+//  Copyright (c) 2014 zx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIProjects/1208_week6_day1/1208_UIButton_calculator/1208_UIButton_calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  1208_UIButton_calculator
+//
+//  Created by zx on 12/9/14.
+//  Copyright (c) 2014 zx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZXAppDelegate class]));
+    }
+}
