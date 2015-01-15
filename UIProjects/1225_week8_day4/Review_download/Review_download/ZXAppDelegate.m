4383e49771660c47dc38d9733bdd6ec8195b49b0
urmyfaith <urmyfaith@qq.com>
2015-01-15 10:10:29 +0800

    订阅通知,添加下载任务

--- UIProjects/1225_week8_day4/Review_download/Review_download/ZXAppDelegate.m
+++ UIProjects/1225_week8_day4/Review_download/Review_download/ZXAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "ZXAppDelegate.h"
+#import "DownloadManager.h"
 
 @implementation ZXAppDelegate
 
@@ -14,36 +15,35 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    
+    btn.frame = CGRectMake(10, 100, 100, 40);
+    btn.backgroundColor = [UIColor grayColor];
+    
+    [btn addTarget:self action:@selector(btnClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [self.window addSubview:btn];
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+-(void)btnClicked:(UIButton *)button{
+    NSLog(@"%s [LINE:%d]", __func__, __LINE__);
+    
+    //1.订阅下载通知
+    [[NSNotificationCenter defaultCenter]addObserver:self
+                                            selector:@selector(downloadFinish:)
+                                                name:@"www.baidu.com"
+                                              object:nil];
+    //2.添加下载任务
+    [[DownloadManager sharedDownloadManager]addDownloadWithDownloadStr:@"www.baidu.com" andDownloadType:0];
+    
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+-(void)downloadFinish:(NSNotification *)notification{
+    
 }
 
 @end
