05d052af53b401f0be316819910953f645025adb
urmyfaith <urmyfaith@qq.com>
2015-01-15 11:14:04 +0800

    init

---
+++ advancedUI/0115_week11_day4/UICollectionView01/UICollectionView01/ZXMainVC.h
@@ -0,0 +1,14 @@
+//
+//  ZXMainVC.h
+//  UICollectionView01
+//
+//  Created by zx on 1/15/15.
+//  Copyright (c) 2015 zx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZXMainVC : UIViewController<UICollectionViewDataSource,UICollectionViewDelegate>
+
+
+@end
