2913839efebe1ba7593bdc435beb0d8b7dc26e99
urmyfaith <urmyfaith@qq.com>
2015-01-19 15:55:01 +0800

    init

---
+++ advancedUI/0119_week12_day1/NSLock/NSLock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSLock
+//
+//  Created by zx on 1/19/15.
+//  Copyright (c) 2015 zx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ advancedUI/0119_week12_day1/NSLock/NSLock/ViewController.m
@@ -0,0 +1,21 @@
+//
+//  ViewController.m
+//  NSLock
+//
+//  Created by zx on 1/19/15.
+//  Copyright (c) 2015 zx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+@end
