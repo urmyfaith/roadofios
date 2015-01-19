69d14f0164dced4bc577fe4c9a1482ebf38bacec
urmyfaith <urmyfaith@qq.com>
2015-01-19 16:11:52 +0800

    NSLock

--- advancedUI/0119_week12_day1/NSLock/NSLock/ViewController.m
+++ advancedUI/0119_week12_day1/NSLock/NSLock/ViewController.m
@@ -13,9 +13,60 @@
 @end
 
 @implementation ViewController
+{
+    NSMutableArray *_dataArray;
+    NSLock *_lock;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _dataArray = [[NSMutableArray alloc] init];
+    
+    _lock = [[NSLock alloc]init];//实例化锁
+    [self performSelectorInBackground:@selector(thread1) withObject:nil];
+    [self performSelectorInBackground:@selector(thread2) withObject:nil];
+}
+
+/**
+ *  --- 生产者
+ *   负责向数组中填充数据
+ *  在操作变量之前,添加锁.
+ *  在操作变量之后,解锁
+ */
+-(void )thread1
+{
+    
+    while ([_dataArray count] < 20) {
+        NSLog(@"%s [LINE:%d] [_dataArray count]=%lu", __func__, __LINE__,
+              (unsigned long)[_dataArray count]);
+        [_lock lock];
+        [_dataArray addObject:@"1"];
+        [_lock unlock];
+        sleep(arc4random()%3);
+    }
+
+}
+
+/**
+ * --- 消费者
+ *  负责向数组中读取数据/删除数据
+ */
+-(void )thread2
+{
+    while (1)
+    {
+        if([_dataArray count] >0)
+        {
+            NSLog(@"%s [LINE:%d] [_dataArray count]=%lu", __func__, __LINE__,
+                  (unsigned long)[_dataArray count]);
+            [_lock lock];
+            [_dataArray removeLastObject];
+            //  [_lock unlock];
+            sleep(arc4random()%3);
+        }
+        
+    }
     
 }
+
 @end
