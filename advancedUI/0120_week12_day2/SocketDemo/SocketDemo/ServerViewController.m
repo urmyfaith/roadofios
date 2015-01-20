cf4d76c84bd4b01ce47a315449a223f469187dcc
urmyfaith <urmyfaith@qq.com>
2015-01-20 15:42:32 +0800

    创建服务器视图控制器

---
+++ advancedUI/0120_week12_day2/SocketDemo/SocketDemo/ServerViewController.m
@@ -0,0 +1,34 @@
+//
+//  ServerViewController.m
+//  SocketDemo
+//
+//  Created by zx on 1/20/15.
+//  Copyright (c) 2015 zx. All rights reserved.
+//
+
+#import "ServerViewController.h"
+
+
+@interface ServerViewController ()
+
+@end
+
+@implementation ServerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
