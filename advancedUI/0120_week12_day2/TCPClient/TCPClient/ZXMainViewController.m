45666d456db56c8e2e9ca590f3e50475b58172b2
urmyfaith <urmyfaith@qq.com>
2015-01-20 10:47:40 +0800

    TCP客户端发送数据

--- advancedUI/0120_week12_day2/TCPClient/TCPClient/ZXMainViewController.m
+++ advancedUI/0120_week12_day2/TCPClient/TCPClient/ZXMainViewController.m
@@ -7,14 +7,16 @@
 //
 
 #import "ZXMainViewController.h"
+#import "AsyncSocket.h"
 
-@interface ZXMainViewController ()
+@interface ZXMainViewController ()<AsyncSocketDelegate>
 
 @end
 
 @implementation ZXMainViewController
 {
     UITextField *_textField;
+    AsyncSocket *_clientSocket;
 }
 
 
@@ -30,6 +32,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    
     
     _textField = [[UITextField alloc]init];
     _textField.frame = CGRectMake(10, 100, 300, 30);
@@ -52,24 +56,76 @@
     btn2.tag = 2;
     [btn2 addTarget:self action:@selector(btnClicked:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn2];
+    
+    
+    _clientSocket = [[AsyncSocket alloc]initWithDelegate:self];
 
     
 }
 
 -(void)btnClicked:(UIButton *)button{
-    NSLog(@"%s [LINE:%d] button.tag=%i", __func__, __LINE__,button.tag);
+   
+    if (button.tag == 1) {
+        NSLog(@"连接服务器");
+        
+
+            //1.判断是否正在连接,如果正在链接,则取消当前链接
+            if (_clientSocket.isConnected) {
+                [_clientSocket disconnect];
+            }
+            
+            //2.链接指定服务器,host,ip
+            //1-1024系统占用,1024-4000是服务占用,4000~65536,
+            //建议:从5000之后开始设置.
+             //NSString *host  = _textField.text;
+             NSString *host = @"10.0.157.78";
+            [_clientSocket connectToHost:host onPort:5678 error:nil];
+
+    }
+    else{
+        NSLog(@"发送数据");
+        
+        // 发送数据
+        if ([_clientSocket isConnected]) {
+            
+            //数据,超时常见,tag值,标志数据
+            //NSString *message = _textField.text;
+            NSString *message = @"echo hello world.";
+            NSData* aData= [message dataUsingEncoding: NSUTF8StringEncoding];
+            [_clientSocket writeData:aData withTimeout:-1 tag:1];
+        }
+    }
+}
+
+
+/**
+ *  当我们连接到服务器使调用此方法
+ *
+ *  @param sock 服务器的sock(包含了端口号和地址)/_clientSocket
+ *  @param host 服务器地址
+ *  @param port 服务器的端口
+ */
+-(void)onSocket:(AsyncSocket *)sock didConnectToHost:(NSString *)host port:(UInt16)port{
+    NSLog(@" 链接到服务器成功");
+    NSLog(@"host-%@",host);
+    NSLog(@"port-%d",port);
+    NSLog(@"sock.connectedHost-%@",sock.connectedHost);
+    NSLog(@"sock.connectedPort-%d",sock.connectedPort);
+    
+    NSLog(@"_clientSocket - %p",_clientSocket);
+    NSLog(@"sock - %p",sock);
+}
+
+//当发送数据成功的时候调用.
+-(void)onSocket:(AsyncSocket *)sock didWriteDataWithTag:(long)tag{
+    NSLog(@"数据发送成功-%ld",tag);
+    NSLog(@"_clientSocket - %p",_clientSocket);
+    NSLog(@"sock - %p",sock);
 }
 
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
     [_textField resignFirstResponder];
 }
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 
 @end
