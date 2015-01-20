951cebed17d4e7f535ceea33661350e66f3fcba2
urmyfaith <urmyfaith@qq.com>
2015-01-20 15:39:13 +0800

    删除调试输出

--- advancedUI/0120_week12_day2/SocketDemo/SocketDemo/MessageItem.m
+++ advancedUI/0120_week12_day2/SocketDemo/SocketDemo/MessageItem.m
@@ -27,7 +27,7 @@
                                                                     options:0
                                                                       error:nil];
     
-    //2.组织根节点
+    
     NSMutableDictionary *dic = [[NSMutableDictionary alloc] init];
 
     
@@ -39,32 +39,9 @@
     [subDic setObject:@"6789" forKey:@"home"];
     [dic setObject:subDic forKey:@"tel"];
     
+    //2.组织根节点
     [self dicToXMLWithDic:dic andXMLElement:[document rootElement]];
-    
-#if 0
-    for (GDataXMLElement *element in document.rootElement.children) {
-        NSLog(@"elementName = %@",element.name);
-        NSLog(@"elementValue = %@",element.stringValue);
-    }
-#else
-    
-    NSData *data = [document XMLData];
-    
-    NSString *string = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
-    
-    NSLog(@"str = %@",string);
-    /*
-     <?xml version="1.0" encoding="utf-8"?>
-     <root>
-     <age>30</age>
-     <name>zx</name>
-     <tel>
-     <phone>12345</phone>
-     <home>6789</home>
-     </tel>
-     </root>
-     */
-#endif
+
     return [document XMLData];
 }
 
