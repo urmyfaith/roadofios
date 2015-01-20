b45756e3889a1501ad64171678539dc7dd9af2b3
urmyfaith <urmyfaith@qq.com>
2015-01-20 14:41:10 +0800

    创建模型

---
+++ advancedUI/0120_week12_day2/SocketDemo/SocketDemo/MessageItem.h
@@ -0,0 +1,26 @@
+//
+//  MessageItem.h
+//  SocketDemo
+//
+//  Created by zx on 1/20/15.
+//  Copyright (c) 2015 zx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+/**
+ *  数据模型
+ *  模型===>二进制数据
+ *  从二进制数据转换成模型属性.
+ */
+@interface MessageItem : NSObject
+
+@property(nonatomic,strong) NSString *messageContent;
+@property(nonatomic,assign) int messageType;
+
+
+-(NSData *)data;
+-(void)parseData:(NSData *)data;
+
+@end
---
+++ advancedUI/0120_week12_day2/SocketDemo/SocketDemo/MessageItem.m
@@ -0,0 +1,13 @@
+//
+//  MessageItem.m
+//  SocketDemo
+//
+//  Created by zx on 1/20/15.
+//  Copyright (c) 2015 zx. All rights reserved.
+//
+
+#import "MessageItem.h"
+
+@implementation MessageItem
+
+@end
