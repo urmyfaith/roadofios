d28395959fe8a9a682eec5f28e01191f2da8c002
urmyfaith <urmyfaith@qq.com>
2015-01-21 12:44:37 +0800

    添加类

---
+++ advancedUI/0120_week12_day2/SocketDemoV2/SocketDemo/ChatViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatViewController.h
+//  SocketDemo
+//
+//  Created by zx on 1/21/15.
+//  Copyright (c) 2015 zx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatViewController : UIViewController
+
+@end
