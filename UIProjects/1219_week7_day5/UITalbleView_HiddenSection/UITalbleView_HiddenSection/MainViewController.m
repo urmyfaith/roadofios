56515d9f2c03f31cf08c03c9e9525300332ce724
urmyfaith <urmyfaith@qq.com>
2015-01-27 21:19:32 +0800

    添加箭头,旋转剪头

--- UIProjects/1219_week7_day5/UITalbleView_HiddenSection/UITalbleView_HiddenSection/MainViewController.m
+++ UIProjects/1219_week7_day5/UITalbleView_HiddenSection/UITalbleView_HiddenSection/MainViewController.m
@@ -17,6 +17,7 @@
     UITableView *_tableView;
     NSMutableArray *_dataArray;
     NSMutableDictionary *_isHiddenSections;
+    NSMutableArray *_isArrowToRight;
 }
 
 - (void)viewDidLoad
@@ -43,6 +44,8 @@
 
 -(void)loadTableViewData{
     
+    _isArrowToRight = [[NSMutableArray alloc]init];
+
     
     _isHiddenSections = [[NSMutableDictionary alloc]init];
     _dataArray = [[NSMutableArray alloc] init];
@@ -53,6 +56,7 @@
             [array addObject:[NSString stringWithFormat:@"%c%d",i,j]];
         }
         [_dataArray addObject:array];
+        [_isArrowToRight addObject:@"NO"];
     }
     
     [_tableView reloadData];
@@ -87,24 +91,33 @@
 
 -(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
     
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-    [button setBackgroundImage:[[UIImage imageNamed:@"section"] stretchableImageWithLeftCapWidth:10 topCapHeight:10]
-                      forState:UIControlStateNormal];
-    [button addTarget:self
-               action:@selector(buttonClicked:)
-     forControlEvents:UIControlEventTouchUpInside];
-    
-    NSString *groupTitle_string = [NSString stringWithFormat:@"第%02ld组",section];
-    [button setTitle:groupTitle_string forState:UIControlStateNormal];
-    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    //button.tag = section + 100;
-    
     UIView *headerView = [[UIView alloc]init];
     headerView.userInteractionEnabled = YES;
     headerView.tag = section + 100;
+    
     UIImageView *backgourndView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 320, 42)];
     backgourndView.image = [[UIImage imageNamed:@"section"] stretchableImageWithLeftCapWidth:10 topCapHeight:10];
     [headerView addSubview:backgourndView];
+    
+    
+
+    UILabel *headTitle = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 60, 40)];
+    headTitle.center = CGPointMake(320/2, 42/2);
+    headTitle.text =  [NSString stringWithFormat:@"第%02ld组",section];
+    [headerView addSubview:headTitle];
+    
+    //26*40
+    UIImage *arrow_image = [UIImage imageNamed:@"right"];
+    UIImageView *rightArrow = [[UIImageView alloc]initWithFrame:
+                               CGRectMake(320-15-26, 1, arrow_image.size.width, arrow_image.size.height)];
+    rightArrow.tag = section + 200;
+    if ([_isArrowToRight[section] isEqualToString:@"YES"]) {
+          rightArrow.image = [UIImage imageNamed:@"right"];
+    }
+    else{
+         rightArrow.image = [UIImage imageNamed:@"down"];
+    }
+    [headerView addSubview:rightArrow];
     
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self
                                                                          action:@selector(headViewTapped:)];
@@ -118,34 +131,15 @@
         NSString *key = [NSString stringWithFormat:@"%ld",((UIView *)tapView).tag];
         if (![_isHiddenSections objectForKey:key]) {
             [_isHiddenSections setObject:@"YES" forKey:key];
+            [_isArrowToRight replaceObjectAtIndex:((UIView *)tapView).tag - 100 withObject:@"YES"];
         }else{
             [_isHiddenSections removeObjectForKey:key];
+            [_isArrowToRight replaceObjectAtIndex:((UIView *)tapView).tag - 100 withObject:@"NO"];
         }
-        [_tableView reloadSections:[NSIndexSet indexSetWithIndex:((UIView *)tapView).tag-100]
-                  withRowAnimation:UITableViewRowAnimationFade];
+        [_tableView reloadSections:[NSIndexSet indexSetWithIndex:((UIView *)tapView).tag-100] withRowAnimation:UITableViewRowAnimationFade];
     }
 }
 
-/**
- *  点击按钮方法
- *  将tag值存入字典.
- *  第一次点击,根据key,没有值====>存入字典===>字典有值===>隐藏对应的分组
- *  字典有值===>从字典删除====>字典没有值===>展开对应的分组.
- *
- *  @param button button
- */
--(void)buttonClicked:(UIButton *)button{
-    
-    NSString *key = [NSString stringWithFormat:@"%ld",button.tag];
-    
-    if (![_isHiddenSections objectForKey:key]) {
-        [_isHiddenSections setObject:@"YES" forKey:key];
-    }else{
-        [_isHiddenSections removeObjectForKey:key];
-    }
-    [_tableView reloadSections:[NSIndexSet indexSetWithIndex:button.tag-100]
-              withRowAnimation:UITableViewRowAnimationFade];
-}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     NSString *key = [NSString stringWithFormat:@"%ld",section+100];
