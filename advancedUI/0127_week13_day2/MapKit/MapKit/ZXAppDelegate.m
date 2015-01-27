8d82bdd480c2dda49968061c05d193245820983e
urmyfaith <urmyfaith@qq.com>
2015-01-27 09:05:58 +0800

    init

---
+++ advancedUI/0127_week13_day2/MapKit/MapKit/ZXAppDelegate.m
@@ -0,0 +1,21 @@
+//
+//  ZXAppDelegate.m
+//  MapKit
+//
+//  Created by zx on 1/27/15.
+//  Copyright (c) 2015 zx. All rights reserved.
+//
+
+#import "ZXAppDelegate.h"
+
+@implementation ZXAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+@end
