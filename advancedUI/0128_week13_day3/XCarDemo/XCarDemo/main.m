075a24608b35dd7330126810bc84c769bed47612
urmyfaith <urmyfaith@qq.com>
2015-01-28 10:09:40 +0800

    init

---
+++ advancedUI/0128_week13_day3/XCarDemo/XCarDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XCarDemo
+//
+//  Created by zx on 1/28/15.
+//  Copyright (c) 2015 zx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZXAppDelegate class]));
+    }
+}
