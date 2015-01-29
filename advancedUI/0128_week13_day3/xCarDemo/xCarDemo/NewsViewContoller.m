d3aab548409bec15104909c5829a535743c10253
urmyfaith <urmyfaith@qq.com>
2015-01-29 14:51:36 +0800

    索引条 添加一像素

--- advancedUI/0128_week13_day3/xCarDemo/xCarDemo/NewsViewContoller.m
+++ advancedUI/0128_week13_day3/xCarDemo/xCarDemo/NewsViewContoller.m
@@ -43,10 +43,32 @@
 -(void)createIndexBar{
     _indexBarView = [[UIView alloc]init];
     _indexBarView.frame = CGRectMake(0, 64, 320, 30);
-    _indexBarView.backgroundColor = [UIColor cyanColor];
+    _indexBarView.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:_indexBarView];
     
-    NSArray *name = @[@"最新",@"新车",@"评测",@"导购",@"详情"];
-    
+    NSArray *names = @[@"最新",@"新车",@"评测",@"导购",@"详情"];
+    for (int i = 0 ; i < names.count; i++) {
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        btn.frame = CGRectMake(i*320/names.count, 0, 320/names.count, _indexBarView.frame.size.height);
+        [btn setTitle:names[i] forState:UIControlStateNormal];
+        [btn setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [btn setTitleColor:[UIColor blueColor] forState:UIControlStateSelected];
+        [btn addTarget:self action:@selector(indexBarClick:) forControlEvents:UIControlEventTouchUpInside];
+        btn.tag = i+1;
+        [_indexBarView addSubview:btn];
+        if (i) {
+            UIView *view = [[UIView alloc]init];
+            view.frame = CGRectMake(i*320/5, 8, 1, 14);
+            view.backgroundColor = [UIColor grayColor];
+            [_indexBarView addSubview:view];
+        }
+        else{
+            btn.selected  = YES;
+        }
+    }
+}
+
+-(void)indexBarClick:(UIButton *)button{
+    NSLog(@"button.tag=%ld",button.tag);
 }
 @end
