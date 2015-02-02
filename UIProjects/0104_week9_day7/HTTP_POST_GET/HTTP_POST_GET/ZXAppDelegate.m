42aa149190a3f48ca241eaa7494888bd0ced5b12
urmyfaith <urmyfaith@qq.com>
2015-02-02 17:02:03 +0800

    post请求,带请求体的方法

--- UIProjects/0104_week9_day7/HTTP_POST_GET/HTTP_POST_GET/ZXAppDelegate.m
+++ UIProjects/0104_week9_day7/HTTP_POST_GET/HTTP_POST_GET/ZXAppDelegate.m
@@ -11,15 +11,18 @@
 @implementation ZXAppDelegate
 {
     NSMutableArray *_array;
-    
+    NSMutableData    *_downloadData;
 }
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     
+    _downloadData = [[NSMutableData alloc]init];
     
-    NSURL *url = [NSURL URLWithString:@"http://192.168.2.2/~zx/php/login.php"];
+    
+   // NSURL *url = [NSURL URLWithString:@"http://192.168.2.2/~zx/php/login.php"];
+    NSURL *url = [NSURL URLWithString:@"http://api.dizhensubao.igexin.com/api.htm"];
     
     //如果是POST请求,则需要是可变的.
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc]initWithURL:url];
@@ -29,7 +32,10 @@
     [request setHTTPMethod:@"POST"];
     
     //向请求体添加参数
-    NSString *str = @"user=zx&password=123";
+   // NSString *str = @"user=zx&password=123";
+    NSString *str = @"{\"action\":\"getDataAction\",\"startTime\": \"1421410377882\",\"endTime\":\"1422706377882\",\"dataSource\": \"CEIC\"}";
+    NSLog(@"%@",str);
+    
     NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding];
     [request setHTTPBody:data];
     
@@ -37,9 +43,35 @@
     [connection start];
     
     
-    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
 }
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data{
+    [_downloadData appendData:data];
+    
+}
+
+-(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response{
+    NSLog(@"收到响应");
+}
+
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection{
+
+#if 0
+    NSString *string = [[NSString alloc]initWithData:_downloadData encoding:NSUTF8StringEncoding];
+    NSLog(@"%s [LINE:%d] string=%@", __func__, __LINE__,string);
+//解析为字符串
+#else
+    
+    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:_downloadData options:NSJSONReadingMutableContainers error:nil];
+    NSLog(@"%s [LINE:%d dic=%@]", __func__, __LINE__,dic);
+//解析为字典
+#endif
+    NSLog(@"done");
+}
+
+
+
+
 @end
