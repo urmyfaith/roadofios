fc275b8eb4102fac470c23407d06730db9ddd429
urmyfaith <urmyfaith@qq.com>
2015-02-03 20:44:28 +0800

    init

---
+++ advancedUI/week14/FashionShow/FashionShow/ZXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZXAppDelegate.h
+//  FashionShow
+//
+//  Created by zx on 15/2/3.
+//  Copyright (c) 2015年 zx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ advancedUI/week14/FashionShow/FashionShowTests/FashionShowTests.m
@@ -0,0 +1,34 @@
+//
+//  FashionShowTests.m
+//  FashionShowTests
+//
+//  Created by zx on 15/2/3.
+//  Copyright (c) 2015年 zx. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FashionShowTests : XCTestCase
+
+@end
+
+@implementation FashionShowTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
